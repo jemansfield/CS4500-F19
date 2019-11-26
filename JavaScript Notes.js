bec08876e6b9198246915fbfe38f5f4d40db1d37
HAWilliams01 <35875559+HAWilliams01@users.noreply.github.com>
2019-11-26 11:59:38 -0500

    Delete JavaScript Notes.js
    Unnecessary file

--- JavaScript Notes.js
+++
@@ -1,9 +0,0 @@
-import Airplane from './airplane';
-
-function displayFuelCapacity() {
-  Airplane.availableAirplanes.forEach(function(element){
-  console.log('Fuel Capacity of ' + element.name + ': ' + element.fuelCapacity);
-  });
-}
-
-displayFuelCapacity();
